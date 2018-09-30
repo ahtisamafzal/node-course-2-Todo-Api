cbabf8854a5adda7d642f4e4ef5fa41afb3a2a8d
AHTISAM AFZAL <ahtisam@ahtisams-mbp.home>
2018-09-30 17:36:07 +0100

    add get test for /todo route with some mock array data

--- tests/server.test.js
+++ tests/server.test.js
@@ -6,8 +6,24 @@ const {
   models
 } = require('../server/server');
 
+const todos = [{
+  text: 'First Test',
+  completed: false,
+  completedAt: 0
+}, {
+  text: 'Second Test',
+  completed: true,
+  completedAt: 1
+}, {
+  text: 'Third Test',
+  completed: false,
+  completedAt: 0
+}]
+
 beforeEach((done) => {
-  models.Todo.deleteMany({}).then(() => done());
+  models.Todo.deleteMany({}).then(() => {
+    return models.Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('GET /todo', () => {
@@ -16,18 +32,19 @@ describe('GET /todo', () => {
     .get('/todo')
     .expect(200)
     .expect((res) => {
-
+      expect(res.body.todos.length).toBe(3);
     })
-    .end((err, res) => {
-      if(err){
-        return done(err);
-      }
-
-      models.Todo.find().then((todos) => {
-        expect(todos.length).toBe(0)
-        done();
-      }).catch((e) => done(e));
-    });
+    .end(done);
+    // .end((err, res) => {
+    //   if(err){
+    //     return done(err);
+    //   }
+    //
+    //   models.Todo.find().then((todos) => {
+    //     expect(todos.length).toBe(3)
+    //     done();
+    //   }).catch((e) => done(e));
+    // });
   });
 });
 
@@ -35,7 +52,7 @@ describe('POST /todo', () => {
   it('Should create a new Todo', (done) => {
 
     var todoModel = new models.Todo({
-      text: 'testing todo by valid passing model -test',
+      text: 'testing todo by valid passing model test',
       completed: false,
       completedAt: 0
     });
@@ -50,7 +67,7 @@ describe('POST /todo', () => {
         if (err) {
           return done(err);
         }
-        models.Todo.find().then((todos) => {
+        models.Todo.find({text: todoModel.text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(todoModel.text);
           expect(todos[0].completed).toBe(todoModel.completed);
@@ -73,7 +90,7 @@ describe('POST /todo', () => {
       });
 
       models.Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(3);
         done();
       }).catch((e) => done(e));
   });
